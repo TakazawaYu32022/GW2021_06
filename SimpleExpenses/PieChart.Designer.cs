b01da254b4a05d8b75ed4c315349b3bd3e9f5213
侑 髙澤 <32022@ojs.ac.jp>
2022-02-09 11:50:06 +0900

    2月9日 簡単家計簿完成

--- SimpleExpenses/PieChart.Designer.cs
+++ SimpleExpenses/PieChart.Designer.cs
@@ -29,9 +29,9 @@ namespace SimpleExpenses
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.ChPieChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.lbPieDay = new System.Windows.Forms.Label();
             this.btBack = new System.Windows.Forms.Button();
@@ -43,22 +43,23 @@ namespace SimpleExpenses
             // 
             // ChPieChart
             // 
-            chartArea2.Name = "ChartArea1";
-            this.ChPieChart.ChartAreas.Add(chartArea2);
-            legend2.Font = new System.Drawing.Font("HG創英角ﾎﾟｯﾌﾟ体", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            legend2.IsTextAutoFit = false;
-            legend2.Name = "Legend1";
-            this.ChPieChart.Legends.Add(legend2);
+            chartArea1.Name = "ChartArea1";
+            this.ChPieChart.ChartAreas.Add(chartArea1);
+            legend1.Font = new System.Drawing.Font("HG創英角ﾎﾟｯﾌﾟ体", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            legend1.IsTextAutoFit = false;
+            legend1.Name = "Legend1";
+            this.ChPieChart.Legends.Add(legend1);
             this.ChPieChart.Location = new System.Drawing.Point(84, 67);
             this.ChPieChart.Name = "ChPieChart";
-            series2.ChartArea = "ChartArea1";
-            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
-            series2.CustomProperties = "PieLabelStyle=Disabled";
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            series2.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
-            series2.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
-            this.ChPieChart.Series.Add(series2);
+            this.ChPieChart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SemiTransparent;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series1.CustomProperties = "PieLabelStyle=Disabled";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series1.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            series1.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            this.ChPieChart.Series.Add(series1);
             this.ChPieChart.Size = new System.Drawing.Size(618, 345);
             this.ChPieChart.TabIndex = 0;
             this.ChPieChart.Text = "chart1";
